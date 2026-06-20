9d8a3f382dea5ce1d925d854d8716d1f0aeca5b3
Dylan Scully <dylan31scully@outlook.com>
2026-06-20 09:58:09 +0100

    feat: finish about page frontend

---
+++ README.md
@@ -0,0 +1,3 @@
+THIS IS SATIRE
+ITS ALL A JOKE
+ITS ALL A JOKE I PROMISE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
--- project/routes/about.py
+++ project/routes/about.py
@@ -4,4 +4,4 @@ about_bp = Blueprint("about", __name__)
 
 @about_bp.route("/")
 def about():
-    return "placeholder"
+    return render_template("about.html")
