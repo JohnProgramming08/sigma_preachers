0f58c11d94bf0e1f3154711c3a2b960c8c2e9e0e
Dylan Scully <dylan31scully@outlook.com>
2026-07-14 13:34:31 +0100

    refactor: remove repetition of header code across HTML files

---
+++ project/static/js/base_header.js
@@ -0,0 +1,30 @@
+const homeLink = document.getElementById("home");
+const profileLink = document.getElementById("profile");
+const searchUsersLink = document.getElementById("search-users");
+const searchRoomsLink = document.getElementById("search-rooms");
+const contactUsLink = document.getElementById("contact-us");
+const adminMessagesLink = document.getElementById("admin-messages");
+
+const path = window.location.pathname;
+let page = path.split("/");
+
+while (["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"].indexOf(page.slice(-1)[0]) > -1) {
+    page.pop();
+}
+page = page.pop();
+
+const mapping = {
+    "home": homeLink,
+    "view_profile": profileLink,
+    "edit_profile": profileLink,
+    "ban_user": profileLink,
+    "promote_user": profileLink,
+    "search_users": searchUsersLink,
+    "search_rooms": searchRoomsLink,
+    "contact_us": contactUsLink,
+    "admin_messages": adminMessagesLink
+}
+
+console.log(page);
+const pageLink = mapping[page];
+pageLink.classList.add("active");
